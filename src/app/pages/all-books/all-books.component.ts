8d8d934828adc4809c4b32de563bba469a651cce
Nadeesha Dassanayake <nadeeshadassanayake96@gmail.com>
2024-02-03 17:35:24 +0530

    Initial Commit

---
+++ src/app/pages/all-books/all-books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-books.component.html',
+  styleUrl: './all-books.component.css'
+})
+export class AllBooksComponent {
+
+}
